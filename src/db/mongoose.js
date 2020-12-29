3467c8e005d7d7737c701fdc29d12a0d342ab77a
johnatanlopes <johnatanlopes@gmail.com>
2020-12-29 07:25:39 -0300

    Validacao e sanatizacao da entrada de dados no mongoose

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 	useNewUrlParser: true,
@@ -8,14 +9,42 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 const User = mongoose.model('User', {
 	name: {
 		type: String,
+		required:  true,
+	},
+	email: {
+		type: String,
+		required: true,
+		validate(value) {
+			if (!validator.isEmail(value)) {
+				throw new Error('Email is invalid')
+			}
+		},
+	},
+	password: {
+		type: String,
+		required: true,
+		minlength: 7,
+		trim: true,
+		validate(value) {
+			if (value.toLowerCase().includes('password')) {
+				throw new Error('Password cannot contain "password"')
+			}
+		},
 	},
 	age: {
 		type: Number,
+		validate(value) {
+			if (value < 0) {
+				throw new Error('Age must be a positive number');
+			}
+		},
 	},
 });
 
 const me = new User({
 	name: 'Johnatan',
+	email: 'johnatanlopes@gmail.com',
+	password: '123456789',
 	age: 31,
 });
 
@@ -28,9 +57,12 @@ me.save().then((result) => {
 const Task = mongoose.model('Task', {
 	description: {
 		type: String,
+		required: true,
+		trim: true,
 	},
 	completed: {
 		type: Boolean,
+		default: false,
 	},	
 });
 
