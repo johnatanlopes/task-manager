8598948179b73319ab0974f5ea8b75d8ba92a0fa
johnatanlopes <johnatanlopes@gmail.com>
2021-01-09 20:00:31 -0300

    Implementado o primeiro teste das tarefas e dividido a inicialização do banco e dados do usuario em um arquivo

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,26 +1,11 @@
-require('dotenv').config({ path: '.test.env' })
+require('dotenv').config({ path: '.test.env' });
 const request = require('supertest');
-const jwt = require('jsonwebtoken');
-const mongoose = require('mongoose');
 
 const app = require('../src/app');
 const User = require('../src/models/user');
+const { userOneId, userOne, setupDatabase } = require('./fixtures/db');
 
-const userOneId = new mongoose.Types.ObjectId();
-const userOne = {
-    _id: userOneId,
-    name: 'Jose',
-    email: 'jose@example.com',
-    password: '90abcd!!',
-    tokens: [{
-        token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET),
-    }],
-};
-
-beforeEach(async () => {
-    await User.deleteMany();
-    await new User(userOne).save();
-});
+beforeEach(setupDatabase);
 
 test('Should signup a new user', async () => {
     const res = await request(app).post('/users').send({
