6bd87b25fe85f6308b1c5297dd4eb6303800fe04
johnatanlopes <johnatanlopes@gmail.com>
2021-01-09 20:21:05 -0300

    Teste de listagem de tarefas e tentativa de deletar tarefa de outro usuário

--- tests/task.test.js
+++ tests/task.test.js
@@ -3,7 +3,15 @@ const request = require('supertest');
 
 const app = require('../src/app');
 const Task = require('../src/models/task');
-const { userOneId, userOne, setupDatabase } = require('./fixtures/db');
+const {
+    userOneId, 
+    userOne, 
+    userTwoId,
+    userTwo,
+    taskOne,
+    taskTwo,
+    setupDatabase 
+} = require('./fixtures/db');
 
 beforeEach(setupDatabase);
 
@@ -20,3 +28,24 @@ test('Should create task for user', async () => {
     expect(task).not.toBeNull();
     expect(task.completed).toEqual(false);
 }); 
+
+test('Should fetch user tasks', async () => {
+    const res = await request(app)
+        .get('/tasks')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200);
+
+    expect(res.body.length).toEqual(2);
+});
+
+test('Should not delete other users tasks', async () => {
+    const res = await request(app)
+        .delete(`/tasks/${taskOne._id}`)
+        .set('Authorization', `Bearer ${userTwo.tokens[0].token}`)
+        .send()
+        .expect(404);
+
+    const task = await Task.findById(taskOne._id);
+    expect(task).not.toBeNull();
+});
