85987c6bf60a1a76256183444d7d6facb34ec773
johnatanlopes <johnatanlopes@gmail.com>
2021-01-08 22:45:06 -0300

    Implementado o teste de autenticacao do usuario e usuario inexistente

--- src/routers/user.js
+++ src/routers/user.js
@@ -32,7 +32,7 @@ router.post('/users/login', async (req, res) => {
 		const token = await user.generateAuthToken();
 		return res.status(200).json({ user, token });
 	} catch (error) {
-		return res.status(500).json(error);
+		return res.status(400).json(error);
 	}
 });
 
--- tests/user.test.js
+++ tests/user.test.js
@@ -1,7 +1,19 @@
 require('dotenv').config({ path: '.test.env' })
-
 const request = require('supertest');
+
 const app = require('../src/app');
+const User = require('../src/models/user');
+
+const userOne = {
+    name: 'Jose',
+    email: 'jose@example.com',
+    password: '90abcd!!',
+};
+
+beforeEach(async () => {
+    await User.deleteMany();
+    await new User(userOne).save();
+});
 
 test('Should signup a new user', async () => {
     await request(app).post('/users').send({
@@ -9,4 +21,18 @@ test('Should signup a new user', async () => {
         email: 'test@exemple.com',
         password: '123456789'
     }).expect(201);
+});
+
+test('Should login existing user', async () => {
+    await request(app).post('/users/login').send({
+        email: userOne.email,
+        password: userOne.password,
+    }).expect(200);
+});
+
+test('Should not login nonexistent user', async () => {
+    await request(app).post('/users/login').send({
+        email: 'non@example.com',
+        password: 'thisisnotmypass',
+    }).expect(400);
 });
